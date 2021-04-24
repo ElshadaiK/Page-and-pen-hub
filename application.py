24531612d11c5dbcb0cb581f3ab1d4ee0088c383
Elshadai <elshadaikassutegegn@gmail.com>
2021-04-24 14:33:09 +0300

    Readme adjusted, and db url structured

--- application.py
+++ application.py
@@ -20,7 +20,10 @@ bcrypt = Bcrypt(app)
 Session(app)
 
 # Set up database
-engine = create_engine('postgresql://zljnywqobtzvwu:58cb0c2defa2db89f6e39949f9ce465888db82bd63be85d08c6c1203dde25643@ec2-54-205-183-19.compute-1.amazonaws.com:5432/d4h7t28tqnk68a')
+dbUrl = os.getenv("DATABASE_URL")
+if(dbUrl[8] == ":"):
+    dbUrl = dbUrl[0:8] + "ql" + dbUrl[8:-1]
+engine = create_engine(dbUrl)
 db = scoped_session(sessionmaker(bind=engine))
 
 ## Helpers
